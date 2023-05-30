f374d0d072c32fa8e94c47e6404dbd9ee2ac4942
shimizu-tomoya <shimitomo.pix@gmail.com>
2023-05-30 18:06:41 +0900

    [add]main,header,footerコンポーネントの追加

---
+++ src/pages/company.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import { Main } from '@/components/Main'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Company() {
+  return (
+    <>
+      <Head>
+        <title>Furniture Design</title>
+      </Head>
+      <Main />
+    </>
+  )
+}
